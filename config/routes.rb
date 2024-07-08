89cbb11189452fbb412e8f1f70a29b3461222839
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-07-08 21:54:09 +0000

    9章投稿機能のモデル作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'about' => 'homes#about'
+  get '/homes/about' => 'homes#about',as:'about'
+  resources :post_images,only: [:new, :index, :show]
+
 end
