dfc2ca69bb70a0c180ac0a936a50ab2282a7c828
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-07-09 21:49:58 +0000

    10章画像一覧機能作成

--- db/migrate/20240704151041_create_postimages.rb
+++ db/migrate/20240704151041_create_postimages.rb
@@ -1,6 +1,6 @@
 class CreatePostimages < ActiveRecord::Migration[6.1]
   def change
-    create_table :postimages do |t|
+    create_table :post_images do |t|
     t.string :shop_name
     t.text :caption
     t.integer :user_id
